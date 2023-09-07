351915d92fa161b2dacc883651044e6d2ae8e2f0
João Lemes <joaolemes17@hotmail.com>
2023-09-06 21:51:48 -0400

    my project 0.1.3

--- site/index2.php
+++ site/index2.php
@@ -12,19 +12,17 @@
 
 <div class="container">
 <h2>Enviando dados</h2>
-<form>
+<form action ="dados.php" method=post>
 <div class="form-group">
-<label for="exampleInputEmail1">Endereço de email</label>
-<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Por favor,digite seu email">
-<small id="emailHelp" class="form-text text-muted">Nunca vamos compartilhar seu email, com ninguém.</small>
+<label for="exampleInputEmail1">Nome:</label>
+<input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nome completo" name="nome">
+<small id="emailHelp" class="form-text text-muted">Nunca vamos compartilhar seu nome, com ninguém.</small>
 </div>
 <div class="form-group">
-<label for="exampleInputPassword1">Senha</label>
-<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha">
+<label for="exampleInputPassword1">Idade:</label>
+<input type="text" class="form-control" id="exampleInputPassword1" placeholder="Sua idade" name="idade">
 </div>
-<div class="form-group form-check">
-<input type="checkbox" class="form-check-input" id="exampleCheck1">
-</div>
+
 <button type="submit" class="btn btn-primary">Enviar</button>
 </form>
 </div>
